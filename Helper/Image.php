20fe357b56c09865b4af3ec0f0405505af25099e
Wilko Nienhaus <wilko.nienhaus@vaimo.com>
2016-04-10 01:00:44 +0200

    add Image Helper replacement

---
+++ Helper/Image.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace MagentoHackathon\Mdia\Helper;
+
+class Image extends Magento\Catalog\Helper\Image
+{
+    public function init($product, $imageId, $attributes = [])
+    {
+        parent::init($product, $imageId, $attributes);
+    }
+}
