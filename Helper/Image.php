5485df265241b8ae834e8375856c4dbf4b067724
Wilko Nienhaus <wilko.nienhaus@vaimo.com>
2016-04-10 02:24:16 +0200

    Generate cloudinary url instead of resizing images

--- Helper/Image.php
+++ Helper/Image.php
@@ -2,10 +2,54 @@
 
 namespace MagentoHackathon\Mdia\Helper;
 
-class Image extends Magento\Catalog\Helper\Image
+class Image extends \Magento\Catalog\Helper\Image
 {
-    public function init($product, $imageId, $attributes = [])
+    protected function getImageFile()
     {
-        parent::init($product, $imageId, $attributes);
+        return 'sample.jpg'; //TODO: remove hardcoded image
+    }
+
+    public function getUrl()
+    {
+        $baseUrl = sprintf('%s/%s',
+            'http://res.cloudinary.com', //TODO: get from config [service-url]
+            'mdia' //TODO: get from config [cloud-name]
+        );
+
+        $transformations = $this->getTransformations();
+
+        $url = $baseUrl;
+        if (!empty($transformations)) {
+            $url .= '/' . implode(',', $transformations);
+        }
+        $url .= '/' . $this->getImageFile();
+
+        return $url;
+    }
+
+    public function getResizedImageInfo()
+    {
+        return null;
+    }
+
+    protected function applyScheduledActions()
+    {
+        //intentional no-op. This prevents creating local resized/cached images.
+    }
+
+    /**
+     * @return array
+     */
+    public function getTransformations()
+    {
+        $sizeParams = array();
+        if (!is_null($this->getWidth())) {
+            $sizeParams[] = 'w_' . $this->getWidth();
+        }
+        if (!is_null($this->getHeight())) {
+            $sizeParams[] = 'h_' . $this->getHeight();
+            return $sizeParams;
+        }
+        return $sizeParams;
     }
 }
